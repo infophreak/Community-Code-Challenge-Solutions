52063605dba0e97b76774fc0683c826c16fe95b8
infophreak <team@infophreak.com>
2025-02-05 11:31:37 -0500

    🟢 l0wk3y solution added

--- Random Quote Generator/l0wk3y/request version.py
+++ Random Quote Generator/l0wk3y/request version.py
@@ -1,18 +1,32 @@
 import requests
 import os
+import random
 from time import sleep
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-response = requests.get('https://zenquotes.io/api/quotes')
-author = response.json()[0]['a']
-quote = response.json()[0]['q']
 
-while True:
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-    response = requests.get('https://zenquotes.io/api/quotes')
-    author = response.json()[0]['a']
-    quote = response.json()[0]['q']
-    print(f'{quote} - {author}')
-    sleep(3)
-
+try:
+    while True:
+        response = requests.get('https://raw.githubusercontent.com/infophreak/Community-Code-Challenge-Solutions/refs/heads/main/Random%20Quote%20Generator/l0wk3y/quotes.json')
+        quotes = response.json()
+        quote = random.choice(quotes)
+        print(f"\r{' ' * 80}", end='', flush=True)  # Clear the line
+        print(f"\r{quote['q']} - {bcolors.OKGREEN}{quote['a']}{bcolors.ENDC}", end='', flush=True)
+        sleep(3)
+except KeyboardInterrupt:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f'\n\n{bcolors.OKGREEN}Goodbye!{bcolors.ENDC}')
+    sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
